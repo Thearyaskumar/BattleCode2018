0c6279a5216412c28bb2c6598ce98154c6a20762
Arya Kumar <2019akumar2@tjhsst.edu>
2018-01-10 22:54:29 -0500

    edited run.sh for silence

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-docker stop $(docker ps -q)
-docker container rm $(docker container ls -aq)
-docker volume rm $(docker volume ls -q)$
-docker volume prune
+docker stop $(docker ps -q) 2> /dev/null
+docker container rm $(docker container ls -aq) 2> /dev/null
+docker volume rm $(docker volume ls -q)$ 2> /dev/null
+docker volume prune -f &> /dev/null
 
 docker run -it --privileged -p 16147:16147 -p 6147:6147 -v "$PWD":/player --rm "battlecode/battlecode-2018"
