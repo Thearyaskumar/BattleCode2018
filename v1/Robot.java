79eb4c3e578b937b3b93ed70cd7f821098066a1b
rubenascoli01 <35244816+rubenascoli01@users.noreply.github.com>
2018-01-12 18:45:40 -0500

    made bug better

--- v1/Robot.java
+++ v1/Robot.java
@@ -20,13 +20,30 @@ public class Robot extends Unit {
         Direction d = unit.location().mapLocation().directionTo(m);
         if (gc.isMoveReady(unit.id())){
             for(int i = 0; i < 8; i++)
-            	if (!gc.canMove(unit.id(), d))
-                    System.out.println("Can't Move!");
-
-            if (gc.canMove(unit.id(), d))
-                gc.moveRobot(unit.id(), d);
-            else
-                System.out.println("I'm stuck :(");
+            	if (gc.canMove(unit.id(),d)){
+                    gc.moveRobot(unit.id(), d);
+                    return;
+                }else{
+                    switch(d){
+                        case Direction.North: d = Direction.Northeast;
+                            break;
+                        case Direction.Northeast: d = Direction.East;
+                            break;
+                        case Direction.East: d = Direction.Southeast;
+                            break;
+                        case Direction.Southeast: d = Direction.South;
+                            break;
+                        case Direction.South: d = Direction.Southwest;
+                            break;
+                        case Direction.Southwest: d = Direction.West;
+                            break;
+                        case Direction.West: d = Direction.Northwest;
+                            break;
+                        case Direction.Northwest: d = Direction.North;
+                            break;
+                    }
+                }
+            System.out.println("I'm stuck :(");
         }
     }
 }
