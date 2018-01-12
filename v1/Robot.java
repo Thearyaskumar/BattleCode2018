4e70dee6b53502381fb9c15f2d93c03caaa0e6c5
Arya Kumar <thearyaskumar@gmail.com>
2018-01-12 09:35:11 -0500

    print robot locations

--- v1/Robot.java
+++ v1/Robot.java
@@ -1,6 +1,7 @@
 import bc.*;
 public class Robot{
 	public static void oneMove(GameController gc, Unit unit, int[] strategy){
-        
+        MapLocation m = unit.location().mapLocation()
+        System.out.println("My Location: (" + m.getX() + ", " + m.getY() + ")");
     }
 }
