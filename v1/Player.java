3dca5e5949c46987b0a8bfbfb3735cc0eba0f0d9
Arya Kumar <2019akumar2@tjhsst.edu>
2018-01-12 18:00:54 -0500

    Began working on bug pathfinding

--- v1/Player.java
+++ v1/Player.java
@@ -3,45 +3,40 @@
 import bc.*;
 
 public class Player {
-    public static void main(String[] args) {
-        // Connect to the manager, starting the game
-        GameController gc = new GameController();
-
-        // This should be called repeatedly, for now we'll do it once
-        int[] strategy = developStrategy(gc);
-
-        while (true) {
-            // No more strategy development for now: eventually, the function will have to be improved a ton
-            VecUnit units = gc.myUnits();
-
-            for (int i = 0; i < units.size(); i++) {
-                Unit unit = units.get(i);
-
-                switch (unit.unitType()){
-                    case Factory: Factory.oneMove(gc, unit, strategy);
-                                  break;
-                    case Rocket: Rocket.oneMove(gc, unit, strategy);
-                                  break;
-                    case Worker: Robot.oneMove(gc, unit, strategy);
-                                  break;
-                    case Ranger: Robot.oneMove(gc, unit, strategy);
-                                  break;
-                    case Mage: Robot.oneMove(gc, unit, strategy);
-                                  break;
-                    case Knight: Robot.oneMove(gc, unit, strategy);
-                                  break;
-                    case Healer: Robot.oneMove(gc, unit, strategy);
-                                  break;
-                }
-            }
-            // Submit the actions we've done, and wait for our next turn.
-            gc.nextTurn();
-        }
-    }
-
-    private static int[] developStrategy(GameController gc){
-	if (gc.planet() == Planet.valueOf("Earth"))
-	    return EarthPlayer.strategy(gc); //The strategy is just: Head over to the enemy
-        return MarsPlayer.strategy(gc); // Haven't even thought about this yet
-    }
+	public static void main(String[] args) {
+		// Connect to the manager, starting the game
+		GameController gc = new GameController();
+		// This should be called repeatedly, for now we'll do it once
+		int[] strategy = developStrategy(gc);
+		while (true) {
+			// No more strategy development for now: eventually, the function will have to be improved a ton
+			VecUnit units = gc.myUnits();
+			for (int i = 0; i < units.size(); i++) {
+				Unit unit = units.get(i);
+				switch (unit.unitType()){
+					case Factory: Factory.oneMove(gc, unit, strategy);
+								 break;
+					case Rocket: Rocket.oneMove(gc, unit, strategy);
+								 break;
+					case Worker: Robot.oneMove(gc, unit, strategy);
+								 break;
+					case Ranger: Robot.oneMove(gc, unit, strategy);
+								 break;
+					case Mage: Robot.oneMove(gc, unit, strategy);
+								 break;
+					case Knight: Robot.oneMove(gc, unit, strategy);
+								 break;
+					case Healer: Robot.oneMove(gc, unit, strategy);
+								 break;
+				}
+			}
+			// Submit the actions we've done, and wait for our next turn.
+			gc.nextTurn();
+		}
+	}
+	private static int[] developStrategy(GameController gc){
+		if (gc.planet() == Planet.valueOf("Earth"))
+			return EarthPlayer.strategy(gc); //The strategy is just: Head over to the enemy
+		return MarsPlayer.strategy(gc); // Haven't even thought about this yet
+	}
 }
--- v1/Robot.java
+++ v1/Robot.java
@@ -2,18 +2,19 @@ import bc.*;
 public class Robot{
     public static void oneMove(GameController gc, Unit unit, int[] strategy){
         MapLocation m = new MapLocation(gc.planet(), strategy[0], strategy[1]);
-	bug(unit, gc, m); // Lazy pathfinding to other side
+	   bug(unit, gc, m); // Lazy pathfinding to other side
     }
+
     public static void bug(Unit unit, GameController gc, MapLocation m){
         // This will be a very rudimentary bathfinding algorithm to see if I can
-	// get it functional at all. This will obv. have to be waaay improved
-	// Find the correct direction:
-	Direction d = unit.location().MapLocation().directionTo(m);
-	if (gc.isMoveReady(unit.id())){
-	    for(int i = 0; i < 8; i++)
-	    	if !gc.canMove(unit.id(), d)
-		    System.out.println("Rotate direction: maybe a switch statement?")
-	// Move here
-	}
+        // get it functional at all. This will obv. have to be waaay improved
+        // Find the correct direction:
+        Direction d = unit.location().mapLocation().directionTo(m);
+        if (gc.isMoveReady(unit.id())){
+            for(int i = 0; i < 8; i++)
+            	if (!gc.canMove(unit.id(), d))
+                    System.out.println("Can't Move!");
+            gc.moveRobot(unit.id(), d);
+        }
     }
 }
