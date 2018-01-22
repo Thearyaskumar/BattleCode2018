317155b315a4965b464778318b994494dd626f33
Naman <presshatdotnet@gmail.com>
2018-01-21 19:36:38 -0500

    hah

--- Ravi_Strategy/Rocket.java
+++ Ravi_Strategy/Rocket.java
@@ -1,6 +1,15 @@
 import bc.*;
 public class Rocket{
     public static void oneMove(GameController gc, Unit unit, int strategy){
-        // This does nothing rn
+        if(unit.structureGarrison().size() == unit.structureMaxCapacity())
+          gc.launchRocket(unit.id(), new MapLocation(Planet.Mars, (int)(Math.random()*gc.startingMap(Planet.Mars).getWidth()), (int)(Math.random()*gc.startingMap(Planet.Mars).getHeight()))
+
+        for(Direction d: Direction.values()){
+          try{
+            Unit u = senseUnitAtLocation(unit.location().mapLocation().add(d))
+            gc.load(unit.id(), u.id());
+          }
+          catch(Exception e){}
+        }
     }
 }
