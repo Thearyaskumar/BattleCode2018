b813f05c70b1ed27a2d62bdcf2b7b4019eef37b1
Naman <presshatdotnet@gmail.com>
2018-01-21 19:12:58 -0500

    fied everything

--- seeding/Knight.java
+++ seeding/Knight.java
@@ -1,10 +1,6 @@
 import bc.*;
 import java.util.*;
 public class Knight extends Robot{
-	private Unit unit;
-	private GameController gc;
 	public Knight(Unit u, GameController gameController){
-		unit = u;
-		gc = gameController;
 	}
 }
