5d4f348b63d58fcf32292877aa44afcf831c1770
rubenascoli01 <35244816+rubenascoli01@users.noreply.github.com>
2018-01-28 17:04:32 -0500

    Copied player code from codeshare

--- seeding/Player.java
+++ seeding/Player.java
@@ -1,7 +1,7 @@
 import bc.*;
 import java.util.*;
 public class Player{
-    public static HashSet<MapLocation> enemyLocs = new HashSet<MapLocation>();
+  	    public static HashSet<MapLocation> enemyLocs = new HashSet<MapLocation>();
   
   public static HashSet<MapLocation> getEnemyLocs() {
     return enemyLocs;
@@ -12,13 +12,13 @@ public class Player{
   public static void addEnemyLoc(MapLocation m) {
     enemyLocs.add(m);
   }
-  public static void main(String[] args){
+	public static void main(String[] args){
         GameController gc = new GameController();
-    //set up variables we'll use later
-        //boolean goToMars = false;
-        //int[] prod = new int[5];
-        OrbitPattern orbits = gc.orbitPattern();
-    
+		//set up variables we'll use later
+      	//boolean goToMars = false;
+      	//int[] prod = new int[5];
+      	OrbitPattern orbits = gc.orbitPattern();
+		
         // Setup hashsets for our units
         HashSet<Worker> myWorkers = new HashSet<Worker>();
         HashSet<Knight> myKnights = new HashSet<Knight>();
@@ -31,12 +31,12 @@ public class Player{
         //UnitType[] attackPriority = [UnitType.Factory,UnitType.Rocket,UnitType.Mage,UnitType.Worker,UnitType.Ranger,UnitType.Knight,UnitType.Healer];
 
         /*public int calcHab(){
-          return -1
+        	return -1
         }
         public int evalMobility(PlanetMap pm){
                 return -1;
         }*/
-        public void determineResearch(boolean EarthIsLarge){
+      	public void determineResearch(boolean EarthIsLarge){
           if (EarthIsLarge){
               gc.queueResearch(UnitType.Worker); //25
               gc.queueResearch(UnitType.Mage);//25
@@ -85,39 +85,43 @@ public class Player{
         //queue all research (hardcoded)
         gc.determineResearch(mapLarge);
         while(1){
-            enemyLocs.clear();
+          	enemyLocs.clear();
             // First we will add all new units:
 
-            // INTEGER: robot.target if
-            // -1: unassigned
+          	// INTEGER: robot.target if
+          	// -1: unassigned
             // 0: Random Exploration
-            // 1: Targeted Moving
-            // 2: Attacking
-            // 3: Retreating
+          	// 1: Targeted Moving
+          	// 2: Attacking
+          	// 3: Retreating
             // <...> Specialized
 
-            // FACTORY: robot.target if
-            // -1: unbuilt
-            // 0: idle
-            // 1: Building - see factory queue
+          	// FACTORY: robot.target if
+          	// -1: unbuilt
+          	// 0: idle
+          	// 1: Building - see factory queue
 
-            // Factory.getQueue():
-            // ArrayList of UnitTypes
+          	// Factory.getQueue():
+          	// ArrayList of UnitTypes
 
-            // ROCKETS: robot.target if
-            // -1: unbuilt
-            // 0: unfull garrison
-            // 1: full garrison - idling for good launch time
-            // 2: Launching
+          	// ROCKETS: robot.target if
+          	// -1: unbuilt
+          	// 0: unfull garrison
+          	// 1: full garrison - idling for good launch time
+          	// 2: Launching
             // 3: launched
           
-            myFactories.clear();
-            myRockets.clear();
+            //WORKER:
+          	// 0: default
+          	// 1: build a factory
+          
+          	myFactories.clear();
+          	myRockets.clear();
             myWorkers.clear();
             myMages.clear();
-            myRangers.clear();
-            myKnights.clear();
-            myHealers.clear();  
+          	myRangers.clear();
+          	myKnights.clear();
+          	myHealers.clear();	
             
             VecUnit units = gc.myUnits();
             for(int i = 0; i < units.size(); i++){
@@ -138,19 +142,19 @@ public class Player{
             //Big mess from here all the way until.......
             /*if(mapLarge){
                 if(!haveSuicideKnighted){
-                    int pos=0;
-                    UnitType[] unitsToBuild = [UnitType.Worker,UnitType.Mage,UnitType.Ranger,UnitType.Knight];
+                  	int pos=0;
+                  	UnitType[] unitsToBuild = [UnitType.Worker,UnitType.Mage,UnitType.Ranger,UnitType.Knight];
                     for(Factory f:myFactories) {
-                      if(f.target==0)
-                            pos = pos%len(unitsToBuild);
-                        f.queue(UnitType.Ranger);
+                    	if(f.target==0)
+                          	pos = pos%len(unitsToBuild);
+                     		f.queue(UnitType.Ranger);
                     }
                 }
                 rocketIfPossible();
                 hab = calcHab();
-                if (!hab){ //or if hab is less than some number
+              	if (!hab){ //or if hab is less than some number
                     goToMars = true;
-                } else if (winning)
+              	} else if (winning)
 
                 production = [UnitType.Worker, UnitType.Mage, UnitType.Ranger, UnitType.Knight, UnitType.Healer];
                 gc.queueReseach(UnitType.Rocket);
@@ -158,28 +162,28 @@ public class Player{
                 // AGRO
                 mob = evalMobility();
                 if (mob){
-                  gc.queueResearch(UnitType.Knight);
+              		gc.queueResearch(UnitType.Knight);
                 }else{
-                    gc.queueResearch(UnitType.Mage);
+                  	gc.queueResearch(UnitType.Mage);
 
                 int mobility = evalMobility();
                   if (mobility==1) {
                     for(Factory f:myFactories) {
-                      if(f.target==0)
-                        f.queue(UnitType.Knight);
+                    	if(f.target==0)
+                     		f.queue(UnitType.Knight);
                     }
                   } else if(mobility==0) {
                     boolean needRockets = 5;
                     if(myMages.size()==0)
-                        needRockets = 0;
+                      	needRockets = 0;
                     else if(myRockets.size()>0)
-                      needRockets = myMages.size()/myRockets.size();
+                    	needRockets = myMages.size()/myRockets.size();
                     for(Factory f : myFactories) {
-                      if(f.target==0)
-                            if(needRockets>0)
-                          f.queue(UnitType.Rocket);
-                            else
-                              f.queue(UnitType.Mage);
+                    	if(f.target==0)
+                          	if(needRockets>0)
+                     			f.queue(UnitType.Rocket);
+                          	else
+                          		f.queue(UnitType.Mage);
                     }
                   }
             }*/
@@ -187,92 +191,99 @@ public class Player{
             // DO THE REST OF THE STUFF HERE
             // Run rocket code
             for(Rocket r : myRockets){
-                switch(r.getTarget()){ //-1-unbuilt, 0-unfull,1-full waiting, 2-launching
-                  case -1: if(r.fullHealth())
-                        r.setTarget(0); //if it's now built, time to work.
-                       break;
-                    case 0:  //sense all units nearby this rocket
-                           //set their bug target to this rocket
+              	switch(r.getTarget()){ //-1-unbuilt, 0-unfull,1-full waiting, 2-launching
+              		case -1: if(r.fullHealth())
+              					r.setTarget(0); //if it's now built, time to work.
+              				 break;
+                  	case 0:  //sense all units nearby this rocket
+                  		     //set their bug target to this rocket
                             for(Direction d: Direction.values()){
                                 try{
                                     if(gc.hasUnitAtLocation(r.location().mapLocation().add(d))) {
-                                        Unit u = gc.senseUnitAtLocation(r.location().mapLocation().add(d));
+                                      try{Unit u = gc.senseUnitAtLocation(r.location().mapLocation().add(d));}catch(Exception e){}
                                         //System.out.println("loaded unit onto rocket");
-                                        gc.load(r.id(), u.id());
+                                      try{gc.load(r.id(), u.id());}catch(Exception e){}
                                     }
-                                    if(r.fullGar())
+                                  	if(r.fullGar())
                                       break;
                                 }
                                 catch(Exception e){
                                     //e.printStackTrace();
                                 }
                             }
-                         
-                         if (r.fullGar() || r.health()<40)
-                          r.setTarget(1); //if it's now full, time to idle
-                         //In Robot code, if type is what r wants, bug toward r's location and load.
-                         break;
-                    case 1:  for (int i = 0; i < 5; i++){ //don't do this more than 10 times per turn to avoid timeOut
-                          if (gc.canLaunchRocket(r.id(),r.getLandingLoc()))
-                            break;
-                          else
-                            r.findLandingLoc();
-                         if (gc.canLaunchRocket(r.id(),r.getLandingLoc()) && (r.health()<40||orbit.duration()<orbit.getCenter()||gc.round()>600))
-                          r.setTarget(2)
-                         break;
-                    }
-                    case 2:  gc.launchRocket(r.id(), r.getLandingLoc()); 
-                         r.setTarget(3);
+                  			 
+                  			 if (r.fullGar() || r.health()<40)
+                               try{r.setTarget(1);}catch(Exception e){} //if it's now full, time to idle
+                  			 //In Robot code, if type is what r wants, bug toward r's location and load.
+                  			 break;
+                  	case 1:  for (int i = 0; i < 5; i++){ //don't do this more than 10 times per turn to avoid timeOut
+                  				if (gc.canLaunchRocket(r.id(),r.getLandingLoc()))
+                  					break;
+                  				else
+                                  try{r.findLandingLoc();}catch(Exception e){}
+                  			 if (gc.canLaunchRocket(r.id(),r.getLandingLoc()) && (r.health()<40||orbit.duration()<orbit.getCenter()||gc.round()>600))
+                               try{r.setTarget(2);}catch(Exception e){}
+                  			 break;
+                  	}
+                  	case 2:  gc.launchRocket(r.id(), r.getLandingLoc()); 
+                  try{r.setTarget(3);}catch(Exception e){}
                              break; //no reason to test if I can... done in case 1
-                    case 3:  if (r.emptyGar())
-                            gc.disintegrateUnit(r.id());
-                          myRockets.remove(r);
-                        else{
+                  	case 3:  if (r.emptyGar())
+                      try{gc.disintegrateUnit(r.id());}catch(Exception e){}
+                  try{myRockets.remove(r);}catch(Exception e){}
+                  			else{
                               if(r.location().isOnPlanet(Planet.Mars)) {
                                 for(Direction d : Direction.values()) {
                                     if(gc.canUnload(r.id(),d))
-                                        gc.unload(r.id(),d);
+                                      try{gc.unload(r.id(),d);}catch(Exception e){}
                                 }
                               }
                             }
                             break;
                 }
-      }
-      if (gc.getTimeLeftMs()<5)
-              gc.nextTurn();
+			}
+			if (gc.getTimeLeftMs()<5)
+              try{gc.nextTurn();}catch(Exception e){}
             for(Factory f : myFactories){
-              f.oneRound();
+              try{f.oneRound();}catch(Exception e){}
             }
-      if (gc.getTimeLeftMs()<5)
-              gc.nextTurn();
+			if (gc.getTimeLeftMs()<5)
+              try{gc.nextTurn();}catch(Exception e){}
             for(Worker w : myWorkers){
-              w.oneRound();
+              int task = 0;
+              if(gc.round()%10 == 0){
+              ////////////TODO: IMPLEMENT TASK (see guide above)
+                if(myFactories.size() < 3 && w.location.isOnPlanet(Planet.Earth)){
+                  task = 1;
+                }
+                try{w.oneRound(task);}catch(Exception e){
+              }
             }
             if (gc.getTimeLeftMs()<5)
-              gc.nextTurn();
-      for(Ranger r : myRangers){
-              r.oneRound();
-      }
+              try{gc.nextTurn();}catch(Exception e){}
+			for(Ranger r : myRangers){
+              try{r.oneRound();}catch(Exception e){}
+			}
             if (gc.getTimeLeftMs()<5)
-              gc.nextTurn();
+              try{gc.nextTurn();}catch(Exception e){}
             for(Mage m : myMages){
-              m.oneRound();
+              try{m.oneRound();}catch(Exception e){}
             }
             if (gc.getTimeLeftMs()<5)
-              gc.nextTurn();
+              try{gc.nextTurn();}catch(Exception e){}
             for(Healer h : myHealers){
-              h.oneRound();
+              try{h.oneRound();}catch(Exception e){}
             }
             if (gc.getTimeLeftMs()<5)
-              gc.nextTurn();
+              try{gc.nextTurn();}catch(Exception e){}
             for(Knight k : myKnights){
-              k.oneRound();
+              try{k.oneRound();}catch(Exception e){}
             }
             gc.nextTurn();
         }
     }
 
-    /*void rocketIfPossible();
+  	/*void rocketIfPossible();
     int calcHab(){
 
     }
