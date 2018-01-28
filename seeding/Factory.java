7de054182db2cbfb5210358931e1399ea3594c9d
rubenascoli01 <35244816+rubenascoli01@users.noreply.github.com>
2018-01-28 17:03:33 -0500

    Factory code completed

--- seeding/Factory.java
+++ seeding/Factory.java
@@ -3,35 +3,70 @@ import java.util.*;
 public class Factory{
     Unit unit;
     GameController gc;
+    int size;
+    double pworker = 0;
+    double pmage = 0;
+    double pranger = 0;
+    double pknight = 0;
+    double phealer = 0;
 	public Factory(Unit u, GameController game){
         unit = u;
         gc = game;
+        size = gc.startingMap(gc.planet()).getHeight()*gc.startingMap(gc.planet()).getWidth();
     }
-
     public void build(UnitType ut){
         /* Begin building units if you are able to */
         if (!gc.canProduceRobot(unit.id(), ut))
             return; // Can't produce robot
         gc.produceRobot(unit.id(), ut);
     }
-
     public int building(){
         return unit.isFactoryProducing() ==0 ? 0 : 1;
     }
-
-    public UnitType unloadGarrison(){
+    public void unloadGarrison(){
         /* Try to unload your garrison in any available spaces */
         VecUnitID garrison = unit.structureGarrison();
-        if (garrison.size() > 0)
-            for(Direction d : Direction.values())
-                if(gc.canUnload(unit.id(), d)){
-                    gc.unload(unit.id(), d);
-                    return gc.unit(garrison.get(0)).unitType();
-                }
-        return null;
+        for(Direction d : Direction.values()){
+            if(gc.canUnload(unit.id(), d))
+                gc.unload(unit.id(), d);
+        }
     }
-
     public boolean canUnloadGarrison(){
         return unit.structureGarrison().size() > 0;
+    }
+    public void oneMove(){
+        if (gc.round()>600 && gc.myUnits().size()>0)
+            return; //save Karbonite
+        if (!building()){
+            if (canUnloadGarrison())
+                unloadGarrison();
+            else{
+                boolean largeMap = size>750;
+                if (largeMap){
+                    pworker = 0.4*((600-gc.round())/600);
+                    phealer = 0.4*(gc.round()/600);
+                    pmage = 0.3;
+                    pranger = 0.2;
+                    pknight = 0.1;
+                }else{
+                    pworker = 0.2*((600-gc.round())/600);
+                    phealer = 0.2*(gc.round()/600);
+                    pmage = 0.4;
+                    pranger = 0.2;
+                    pknight = 0.2;
+                rand = random();
+                if (rand<pworker)
+                    build(UnitType.Worker); 
+                else if (rand<pworker+phealer)
+                    build(UnitType.Healer);
+                else if (rand<pworker+phealer+pmage)
+                    build(UnitType.Mage);
+                else if (rand<pworker+phealer+pmage+pranger)
+                    build(UnitType.Ranger);
+                else
+                    build(UnitType.Knight);
+                }
+            }
+        }
     }
 }
